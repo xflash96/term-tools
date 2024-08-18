391f2919721bfdb45333baf652a9301c3435de34
Po-Wei Wang <xflash96@gmail.com>
2024-08-18 12:41:09 -0700

    fix python3.8 ensurepip on debian

--- installers/vim.sh
+++ installers/vim.sh
@@ -16,6 +16,17 @@ if [ -e ~/.vimrc ]; then
     exit 1
 fi
 
+sudo chown -R $USER $HOME/.local $HOME/.config
+
+# fix the missing ensurepip in ubuntu python3.8 required by mason
+python3_subver=$(python3 -c 'import sys; print(sys.version_info[1])')
+if [ $python3_subver -eq 8 ] && ! python3.8 -m ensurepip --version; then
+    curl -fL http://ports.ubuntu.com/pool/universe/p/python3.8/python3.8-venv_3.8.10-0ubuntu1~20.04.11_arm64.deb -o ensurepip.deb
+    ar x ensurepip.deb data.tar.xz
+    tar xf data.tar.xz && sudo mv ./usr/lib/python3.8/ensurepip /usr/lib/python3.8/ensurepip
+    rm -rf ensurepip.deb data.tar.xz ./usr
+fi
+
 # Install dotfiles (this will fail it already exists so we are safe)
 ln -s $TERM_TOOLS/config/vimrc-minimal ~/.vimrc
 mkdir -p ~/.config/nvim \
